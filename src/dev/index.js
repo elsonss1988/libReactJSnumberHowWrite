ab94f498db9b05252c373dcc5841a917a78c7b48
elsonss1988 <elsonss1988@gmail.com>
2020-10-08 20:19:09 -0300

    iniciando biblioteca em ReactJS

---
+++ src/dev/index.js
@@ -0,0 +1,21 @@
+import React,{useState} from "react";
+import ReactDOM from "react-dom";
+import ReactInputPorExtenso from "../lib";
+
+const App=()=>{
+    const [numero,setNumero]=useState("");
+    return (
+        <>
+        <ReactInputPorExtenso 
+        tipoExtenso="monetario"
+        onChange={numeroExtenso=>setNumero(numeroExtenso)}
+        />
+        <p>
+            <strong>Numero por extenso</strong>: {numero}
+        </p>
+        </>
+
+    )
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
---
+++ src/lib/reactporextenso.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import numero from "numero-por-extenso";
+
+const ReactInputPorExtenso=({tipoExtenso, onChange, ...props})=>(
+    <input 
+        type="number" 
+        onChange={(e)=>{
+        const numeroPorExtenso = numero.porExtenso(
+            e.target.value,
+            numero.estilo[tipoExtenso]
+            );
+        onChange(numeroPorExtenso, e);
+        }}
+        {...props}
+    />
+);
+    ReactInputPorExtenso.propTypes={
+        tipoExtenso: PropTypes.oneOf(['normal','monetario','porcetagem'])
+    }
+    ReactInputPorExtenso.defaultPropsTypes={
+        tipoExtenso:"normal"
+    }
+    export default  ReactInputPorExtenso;
